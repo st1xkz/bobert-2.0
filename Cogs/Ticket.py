3feae588ca36bffe4c2972abed152d7e0eaa6c97
st1xkz <83383474+st1xkz@users.noreply.github.com>
2022-01-15 19:23:58 +0000

    added an embed to ticket.py

--- Cogs/Ticket.py
+++ Cogs/Ticket.py
@@ -36,17 +36,24 @@ class Ticket(commands.Cog):
 
                 channel = guild.get_channel(channel_id)
 
-                ticket_channel = await category.create_text_channel(f"ticket-{payload.member.display_name}", topic=f"A ticket for {payload.member.display_name}.", permission_synced=True)
+                ticket_channel = await category.create_text_channel(f"ticket-{payload.member.name}", topic=f"A ticket for {payload.member.name}#{payload.member.discriminator}.", permission_synced=True)
                 
                 await ticket_channel.set_permissions(payload.member, read_messages=True, send_messages=True)
 
                 message = await channel.fetch_message(msg_id)
                 await message.remove_reaction(payload.emoji, payload.member)
 
-                await ticket_channel.send(f"{payload.member.mention} Thank you for creating a ticket! Use **'?close'** to close your ticket.")
+                embed = discord.Embed(
+                    title="Thanks for Requesting Support!",
+                    description=f"Hey {payload.member.display_name}, this is your ticket! Please state what you made this ticket for, any issues you are experiencing and/or what you have tried and a staff member will be with you as soon as they can.\n\n**Remember to:**\n • Know that **no one** is obligated to answer you if they feel that you are trolling or misusing this ticket system.\n • **Make sure** to be as clear as possible when explaining and provide as many details as you can.\n • **Be patient** as we (staff members) have our own lives *outside of Discord* and we tend to get busy most days. We are human, so you should treat us as such!\n\nAbusing/misusing this ticket system may result in punishment that varies from action to action.",
+                    color=payload.member.guild.me.color,
+                    timestamp=datetime.utcnow()
+                )
+                embed.set_footer(text="Closes after a period of inactivity, or when you or a staff member type *close", icon_url=payload.member.avatar_url)
+                await ticket_channel.send(f"{payload.member.mention}", embed=embed)
 
                 try:
-                    await self.client.wait_for("message", check=lambda m: m.channel == ticket_channel and m.author == payload.member and m.content == "?close", timeout=3600)
+                    await self.client.wait_for("message", check=lambda m: m.channel == ticket_channel and m.author == payload.member and m.content == "*close", timeout=30)
 
                 except asyncio.TimeoutError:
                     await ticket_channel.delete()
@@ -54,10 +61,10 @@ class Ticket(commands.Cog):
                 else:
                     await ticket_channel.delete()
 
-    @commands.command(aliases=['ctic'])
-    async def configticket(self, ctx, msg: discord.Message=None, category: discord.CategoryChannel=None):
+    @commands.command(aliases=['ct'])
+    async def createticket(self, ctx, msg: discord.Message=None, category: discord.CategoryChannel=None):
         if msg is None or category is None:
-            await ctx.channel.send("Failed to configure the ticket as an argument was not given or was invalid.")
+            await ctx.channel.send("Failed to configure the ticket as an argument was not given or was invalid.", delete_after=60)
             return
 
         self.client.ticket_configs[ctx.guild.id] = [msg.id, msg.channel.id, category.id] # this resets the configuration
@@ -73,15 +80,15 @@ class Ticket(commands.Cog):
                     await file.write(line)
 
         await msg.add_reaction("🎫")
-        await ctx.channel.send(f"✅ Successfully configured ticket system for **{ctx.guild.name}**.")
+        await ctx.channel.send(f"✅ Successfully configured ticket system for **{ctx.guild.name}**.", delete_after=60)
 
     @commands.command(aliases=['tcon'])
-    async def ticket_config(self, ctx):
+    async def ticketconfig(self, ctx):
         try:
             msg_id, channel_id, category_id = self.client.ticket_configs[ctx.guild.id]
 
         except KeyError:
-            await ctx.channel.send("You have not configured the ticket system yet.")
+            await ctx.channel.send("You have not configured the ticket system yet.", delete_after=60)
 
         else:
             embed = discord.Embed(
